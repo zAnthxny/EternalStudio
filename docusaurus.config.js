d98018ae1cea30f20960aacb2e355827b45db7e4
zAnthxny <xdanthony107@gmail.com>
2026-04-12 00:38:14 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,1 +1,99 @@
+import { themes as prismThemes } from 'prism-react-renderer';
 
+const config = {
+  title: 'NovaRestore Wiki',
+  tagline: 'Documentación oficial de NovaRestore',
+  favicon: 'img/logo.png',
+
+  future: {
+    v4: true,
+  },
+
+  url: 'https://zanthxny.github.io',
+  baseUrl: '/',
+
+  organizationName: 'zanthxny',
+  projectName: 'novarestore-wiki',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/logo.png',
+    navbar: {
+      title: 'NovaRestore',
+      logo: {
+        alt: 'NovaRestore Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentación',
+        },
+        {
+          href: 'https://github.com/zanthxny/novarestore-wiki',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introducción',
+              to: '/intro',
+            },
+            {
+              label: 'Instalación',
+              to: '/instalacion',
+            },
+          ],
+        },
+        {
+          title: 'Comunidad',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/zanthxny/novarestore-wiki',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} NovaRestore.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
+};
+
+export default config;
